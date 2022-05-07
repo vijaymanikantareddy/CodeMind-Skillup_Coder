8f50eb2d9c14031038fcccb956500edb98bf6c71
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-05-07 19:16:03 +0530

    Create count odd numbers between even numbers.cpp

---
+++ Level 12/count odd numbers between even numbers.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, a, cnt=0;
+    vector<int> arr;
+    cin>>n;
+    for(int i=0 ; i<n; i++){
+        cin>>a;
+        arr.push_back(a);
+    }
+    for(int i=1 ; i<n-1 ; i++){
+        if(arr[i]%2!=0 and arr[i+1]%2==0 and arr[i-1]%2==0){
+            cnt++;
+        }
+    }
+    cout<<cnt;
+}
