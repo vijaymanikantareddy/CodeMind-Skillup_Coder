6a4b7b897b95352f3eddb94a6a3cfc219f848fa2
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-05-07 19:48:55 +0530

    Create unique odd elements sum.cpp

---
+++ Level 18/unique odd elements sum.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a,sum=0;
+    cin>>n;
+    vector<int> arr;
+    for(int i=0 ; i<n ; i++){
+        cin>>a;
+        arr.push_back(a);
+    }
+    for(int i=0 ; i<n ; i++){
+        for(int j=i+1 ; j<n ; j++){
+            if(arr[i]==-1)
+                continue;
+            if(arr[i]==arr[j]){
+                arr[j]=-1;
+            }
+        }
+        if(arr[i]==-1)
+            continue;
+        if(arr[i]%2!=0)
+            sum+=arr[i];
+    }
+    cout<<sum;
+}
