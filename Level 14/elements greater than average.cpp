edf4c49f86ecb657d484259eb6022496d66523ba
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-05-07 19:35:51 +0530

    Create elements greater than average.cpp

---
+++ Level 14/elements greater than average.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n, avg=0, cnt=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0 ; i<n ; i++){
+        cin>>arr[i];
+        avg+=arr[i];
+    }
+    avg /= n;
+    for(int i=0 ; i<n ; i++){
+        if(arr[i]>=avg) 
+            cnt++;
+    }
+    cout<<cnt;
+}
