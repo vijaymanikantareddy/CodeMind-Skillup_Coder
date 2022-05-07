009eb745fc0535743e91b27f23ecd06f8f6dc870
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-05-07 19:02:17 +0530

    Create Even Sum.cpp

---
+++ Level 9/Even Sum.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, a, sum=0;
+    vector<int> arr;
+    cin>>n;
+    for(int i=0 ; i<n; i++){
+        cin>>a;
+        arr.push_back(a);
+    }
+    for(int i=0 ; i<arr.size() ; i++){
+        if(arr[i]%2==0){
+            sum+=arr[i];
+        }
+    }
+    cout<<sum;
+}
