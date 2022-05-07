69f29ae0e672869a3f1505e382b6a86bc74396b1
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-05-07 19:34:49 +0530

    Create maximum of numbers not between A and B.cpp

---
+++ Level 14/maximum of numbers not between A and B.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, max=-1, a, b;
+    vector<int> arr;
+    cin>>n;
+    for(int i=0 ; i<n ;i++){
+        int a;
+        cin>>a;
+        arr.push_back(a);
+    }
+    cin>>a>>b;
+    for(int i=0 ; i<n ; i++){
+        if(arr[i]>=a and arr[i]<=b){
+            
+        }
+        else{
+            max = max>arr[i]? max: arr[i];
+        }
+    }
+    cout<<max;
+}
