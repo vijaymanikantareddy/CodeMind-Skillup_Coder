2d8c3ef0ba365ef8fafe9df1c870bc7a612888c4
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-05-07 19:10:50 +0530

    Create last odd number in an array.cpp

---
+++ Level 11/last odd number in an array.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n, a;
+    vector<int> arr;
+    cin>>n;
+    for(int i=0 ; i<n; i++){
+        cin>>a;
+        arr.push_back(a);
+    }
+    for(int i=arr.size()-1 ; i>=0 ; i--){
+        if(arr[i]%2!=0){
+            cout<<arr[i];
+            break;
+        }
+    }
+}
