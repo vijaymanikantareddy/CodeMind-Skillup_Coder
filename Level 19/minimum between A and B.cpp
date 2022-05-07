d47834487228c69aab060d2fe0ee7e31a63205bb
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-05-07 19:50:31 +0530

    Create minimum between A and B.cpp

---
+++ Level 19/minimum between A and B.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n, min=9999, cnt=0, a, b;
+    vector<int> arr;
+    cin>>n;
+    for(int i=0 ; i<n ;i++){
+        int a;
+        cin>>a;
+        arr.push_back(a);
+    }
+    cin>>a>>b;
+    for(int i=0 ; i<n ; i++){
+        if(arr[i]>=a and arr[i]<=b){
+            min = min<arr[i]? min: arr[i];
+            cnt++;
+        }
+    }
+    if(cnt>0)
+        cout<<min;
+    else
+        cout<<-1;
+}
