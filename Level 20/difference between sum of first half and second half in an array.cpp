2a02476e7bb58a26df6d64ac8a9126725b372a83
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-05-07 19:54:07 +0530

    Create difference between sum of first half and second half in an array.cpp

---
+++ Level 20/difference between sum of first half and second half in an array.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, suma=0, sumb=0;
+    cin>>n;
+    int arr[n];
+    for(int i=0 ; i<n ; i++){
+        cin>>arr[i];
+        if(i<(n/2)){
+            suma+= arr[i];
+        }
+        else{
+            sumb += arr[i];
+        }
+    }
+
+    cout<<abs(suma-sumb);
+}
