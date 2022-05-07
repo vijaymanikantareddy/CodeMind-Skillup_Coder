4c059a993af9e6ccb32267665ba87738132e5d86
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-05-07 19:40:31 +0530

    Create Strictly EVEN.cpp

---
+++ Level 15/Strictly EVEN.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,b,a, cnt=0;
+    vector<int> arr;
+    cin>>n;
+    bool status = true;
+    for(int i=0 ; i<n ; i++){
+        int inp;
+        cin>>inp;
+        arr.push_back(inp);
+    }
+    for(int i=0 ; i<n ; i++){
+        if(arr[i]%2==0 and i%2!=0){
+            status = false;
+        }
+    }
+    status? cout<<"True":cout<<"False";
+}
