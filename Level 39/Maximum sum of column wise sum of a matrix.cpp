ae66e884c9e028fddb54ac625ecfddbccb042d0f
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-05-07 20:21:20 +0530

    Create Maximum sum of column wise sum of a matrix.cpp

---
+++ Level 39/Maximum sum of column wise sum of a matrix.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m, sum=0;
+    vector<vector<int>> arr;
+    cin>>m>>n;
+    int sumar[n]={0};
+    for(int i=0 ; i<m ; i++){
+        vector<int> row;
+        for(int j=0 ; j<n ; j++){
+            int inp;
+            cin>>inp;
+            row.push_back(inp);
+        }
+        arr.push_back(row);
+    }
+    for(int i=0 ; i<m ; i++){
+        for(int j=0 ; j<n ; j++){
+            sumar[j]+=arr[i][j];
+        }
+    }
+    int max = sumar[0];
+    for(int i=1 ; i<n ; i++)
+        max = max>sumar[i]? max: sumar[i];
+    cout<<max;
+}
