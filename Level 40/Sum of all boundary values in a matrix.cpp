79ea2569c97e54f7f12e38518450a88fc1a0d95e
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-05-07 20:33:11 +0530

    Create Sum of all boundary values in a matrix.cpp

---
+++ Level 40/Sum of all boundary values in a matrix.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,m, sum=0;
+    vector<vector<int>> arr;
+    cin>>m>>n;
+    for(int i=0 ; i<m ; i++){
+        vector<int> row;
+        for(int j=0 ; j<n ; j++){
+            int inp;
+            cin>>inp;
+            row.push_back(inp);
+        }
+        arr.push_back(row);
+    }
+    for(int i=0 ; i<m ; i++){
+        for(int j=0 ; j<n ; j++){
+            if(i==0 or j==n-1 or j==0 or i==n-1){
+                sum+=arr[i][j];
+            }
+        }
+    }
+    cout<<sum;
+}
