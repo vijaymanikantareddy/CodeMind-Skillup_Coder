4e339a604a5d4c9e780abb3583e8ef4464b6de78
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-05-07 20:18:53 +0530

    Create Absolute difference of two matrices.cpp

---
+++ Level 38/Absolute difference of two matrices.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n;
+    vector<vector<int>> arr1, arr2, res;
+    cin>>n;
+    for(int i=0 ; i<n ; i++){
+        vector<int> row;
+        for(int j=0 ; j<n ; j++){
+            int inp;
+            cin>>inp;
+            row.push_back(inp);
+        }
+        arr1.push_back(row);
+    }
+    for(int i=0 ; i<n ; i++){
+        vector<int> row;
+        for(int j=0 ; j<n ; j++){
+            int inp;
+            cin>>inp;
+            row.push_back(inp);
+        }
+        arr2.push_back(row);
+    }
+    for(int i=0 ; i<n ; i++){
+        vector<int> row;
+        for(int j=0 ; j<n ; j++){
+            int diff = abs(arr1[i][j]-arr2[i][j]);
+            row.push_back(diff);
+        }
+        res.push_back(row);
+    }
+    for(int i =0 ; i<n ; i++){
+        for(int j=0 ; j<n ; j++){
+            cout<<res[i][j];
+            if(j<n-1)
+                cout<<" ";
+        }
+        if(i<n-1)
+            cout<<"\n";
+    }
+}
