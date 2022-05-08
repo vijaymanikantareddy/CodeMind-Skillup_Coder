b75e4a702bb3e7e5d6f39c70ff51f930f212b398
Vijay Manikanta Reddy Sathi <83952736+vijaymanikantareddy@users.noreply.github.com>
2022-05-08 06:13:08 +0530

    Create lowercase letters in a string.cpp

---
+++ Level 30/lowercase letters in a string.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std; 
+int main()
+{
+    string s;
+    getline(cin, s);
+    int cnt=0;
+    for(int i=0 ; i<s.size() ; i++){
+        if(islower(s[i])){
+            cnt++;
+        }
+    }
+    cout<<cnt;
+}
